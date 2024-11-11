6700a12e20cbe93deccc2420d0a18c428b34108a
Vitor José <vitorvsgp@gmail.com>
2024-11-11 11:28:49 -0300

    atributos de jogador e time

---
+++ Jogador.py
@@ -0,0 +1,6 @@
+class Jogador:
+    
+    def __init__(self, nome, numero, posicao):
+        self.nome = nome
+        self.numero = numero
+        self.posicao = posicao
---
+++ Time.py
@@ -0,0 +1,7 @@
+class Time:
+    
+    def __init__(self, nome, jogadores, tecnico, casa):
+        self.nome = nome
+        self.jogadores = jogadores
+        self.tecnico = tecnico
+        self.casa = casa
