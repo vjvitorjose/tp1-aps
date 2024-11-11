a9de728d30df2d924233321a186d7330e14cdae1
José Vitor Santos Alves <josevitorsantosalves00@gmail.com>
2024-11-11 12:15:39 -0300

    desenvolvimento da classe jogador

--- Jogador.py
+++ Jogador.py
@@ -1,6 +1,13 @@
 class Jogador:
     
     def __init__(self, nome, numero, posicao):
-        self.nome = nome
-        self.numero = numero
-        self.posicao = posicao
+        self.__nome = nome
+        self.__numero = numero
+        self.__posicao = posicao
+    
+    def exibir_info(self):
+        info = (
+            f"Nome: {self.__nome}\n"
+            f"Numero: {self.__numero}\n"
+            f"Posição: {self.__posicao}\n"
+        )
